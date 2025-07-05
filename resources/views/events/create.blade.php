9d95a1b87d25a955ed909f36a851c3f756a3ec11
NetoPetit <cidao_petit5@hotmail.com>
2025-07-05 17:02:43 -0300

    save no banco e flash msg

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,35 @@
 @section('title', 'Criar Evento')
 
 @section('content')
-    <h1>Crie um evento</h1>
+
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie o seu evento</h1>
+        <form action="/events" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="image">Imagem do evento:</label>
+                <input type="file" name="image" id="image" class="form-control-file">
+            </div>
+            <div class="form-group">
+                <label for="title">Evento:</label>
+                <input type="text" name="title" id="title" class="form-control" placeholder="Nome do evento">
+            </div>
+            <div class="form-group">
+                <label for="city">Cidade:</label>
+                <input type="text" name="city" id="city" class="form-control" placeholder="Nome da cidade">
+            </div>
+            <div class="form-group">
+                <label for="private">O evento é privado?</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="description">Descrição:</label>
+                <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento"></textarea>
+            </div>
+            <input type="submit" class="btn btn-primary" value="Criar Evento">
+        </form>
+    </div>
 @endsection
